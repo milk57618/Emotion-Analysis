d3085c623ea9be51818d0e14270ea09c1393bdb3
SUDY-DESKTOP\milk5 <milk57618@gmail.com>
2017-09-27 19:42:47 +0900

    Add : Cognitive + Xamarin, Azure Function + Xamarin Test
    Signed-off-by: SUDY-DESKTOP\milk5 <milk57618@gmail.com>

---
+++ TestFile/test122121/test122121/MainActivity.cs
@@ -0,0 +1,114 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using System;
+using System.Collections.Generic;
+using Android.Content;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace test122121
+{
+    [Activity(Label = "test122121", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        static TextView txt;
+        static ListView lstVehicles;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+          
+            txt = FindViewById<TextView>(Resource.Id.titleText);
+            lstVehicles = FindViewById<ListView>(Resource.Id.vehicleslist);
+            txt.Text = "Testing Azure Functions from Xamarin!";
+
+            ActiveAzureFunction();
+        }
+
+        public async void ActiveAzureFunction()
+        {
+            VehiclesAzureFunction vehiclesAzureFunction = new VehiclesAzureFunction("https://rcamerafunction.azurewebsites.net");
+            var vehiclesList = await vehiclesAzureFunction.GetVehiclesHardCodedAsync();
+            VehicleAdapter vadpt;
+            vadpt = new VehicleAdapter(this, vehiclesList);
+            lstVehicles.Adapter = vadpt;
+        }
+
+        public class VehiclesAzureFunction
+        {
+            private string v;
+
+            public VehiclesAzureFunction() { }
+
+            public VehiclesAzureFunction(string v)
+            {
+                this.v = v;
+            }
+
+            public async Task<List<Vehicle>> GetVehiclesHardCodedAsync()
+            {
+                var client = new System.Net.Http.HttpClient();
+                string url = $"https://rcamerafunction.azurewebsites.net/api/HttpTriggerCSharp1?code=jIdXBMbxiysszZ4QdWPyZU3CLrvdwzu7vGQVardmVivzIV7qxsFJww==&make=Chevrolet";
+                var response = await client.GetAsync(url);
+                var vehiclesJson = response.Content.ReadAsStringAsync().Result;
+                List<Vehicle> listOfVehicles = JsonConvert.DeserializeObject<List<Vehicle>>(vehiclesJson);
+                return listOfVehicles;
+            }
+        }
+    }
+    
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+    }
+    public class VehicleAdapter : BaseAdapter<Vehicle>
+    {
+        public List<Vehicle> vList;
+        private Context vContext;
+        public VehicleAdapter(Context context, List<Vehicle> list)
+        {
+            vList = list;
+            vContext = context;
+        }
+        public override Vehicle this[int position]
+        {
+            get { return vList[position]; }
+        }
+        public override int Count
+        {
+            get { return vList.Count; }
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            try
+            {
+                if (row == null)
+                {
+                    row = LayoutInflater.From(vContext).Inflate(Resource.Layout.Main, null, false);
+                }
+                TextView txtModel = row.FindViewById<TextView>(Resource.Id.titleText);
+                txtModel.Text = vList[position].Model;
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+            finally { }
+            return row;
+        }
+    }
+}
+
